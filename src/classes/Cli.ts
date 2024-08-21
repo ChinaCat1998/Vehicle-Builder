a79c34eb60d3a9cf84cbfef8420d53ce85914d08
Catrina_Evilsizer <tranqcalypso@gmail.com>
2024-08-21 19:23:21 -0400

    updates

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -261,7 +261,7 @@ class Cli {
             new Wheel(parseInt(answers.frontWheelDiameter), answers.frontWheelBrand),
             new Wheel(parseInt(answers.rearWheelDiameter), answers.rearWheelBrand),
           ]
-        );
+      )};
           this.vehicles.push(motorbike);
           this.selectedVehicleVin = motorbike.vin;
           this.performActions();
@@ -269,7 +269,7 @@ class Cli {
         // TODO: push the motorbike to the vehicles array
         // TODO: set the selectedVehicleVin to the vin of the motorbike
         // TODO: perform actions on the motorbike
-      )});
+      )};
   }
   // method to find a vehicle to tow
   // TODO: add a parameter to accept a truck object
